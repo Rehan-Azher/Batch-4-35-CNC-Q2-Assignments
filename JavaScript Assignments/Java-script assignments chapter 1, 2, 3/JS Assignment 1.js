74bf6bb359e63c99891f68ef9017a4471a8aa3cd
Rehan-Azher <rehanazher02@gmail.com>
2021-01-22 17:19:13 +0500

    After Adding Javcascript Assignments

---
+++ JavaScript Assignments/Java-script assignments chapter 1, 2, 3/JS Assignment 1.js
@@ -0,0 +1,5 @@
+var firstName = "Rehan";
+var secondName = "Azhar";
+var age = 45;
+
+window.alert("Hello " + firstName + " " + secondName + ", You are " + age + " years old");
---
+++ JavaScript Assignments/Java-script assignments chapter 10 to 14/JS Assignment 4.js
@@ -0,0 +1,30 @@
+var num_1 = prompt("Enter First Number: ");
+var num_2 = prompt("Enter Second Number: ");
+var opr = prompt("Enter oprator: ");
+var ans;
+if (opr=="+") {
+    ans = parseInt(num_1) + parseInt(num_2);
+} else if (opr=="-") {
+    ans = num_1 - num_2;
+} else if (opr=="*") {
+    ans = num_1 * num_2;
+} else if (opr=="/") {
+    ans = num_1 / num_2;
+} else {
+    ans = num_1 % num_2;
+}
+
+window.alert(num_1 +" "+ opr +" "+ num_2 +" = " + ans);
+
+
+var a = prompt("Enter Number in Eng: ");
+var b = prompt("Enter Number in Urdu: ");
+var c = prompt("Enter Number in Islamiat: ");
+var d = prompt("Enter Number in Pakistan Studies: ");
+var e = prompt("Enter Number in Maths: ");
+
+var sum = parseInt(a)+parseInt(b)+parseInt(c)+parseInt(d)+parseInt(e);
+var t_marks = 500;
+var per = (sum/t_marks)*100;
+
+window.alert(per);
