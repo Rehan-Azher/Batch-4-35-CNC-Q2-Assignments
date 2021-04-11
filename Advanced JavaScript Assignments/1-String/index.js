63e58ae8943f3d6d9d14ea4777a46c57646cb007
Rehan-Azher <rehanazher02@gmail.com>
2021-04-11 14:17:59 +0500

    Advanced JS 1

---
+++ Advanced JavaScript Assignments/1-String/index.js
@@ -0,0 +1,14 @@
+// Assignment
+
+// create a varibale name "str1"
+// Assign a value to "str1 varibale"
+// create multi-line template using "JS String" -- use some dummy text here ```
+// print str1 value in "console.log()" ```multi-line dummy text```
+
+var str1 = 'Rehan Azher';
+
+console.log (str1 + ' Testing Javascript code');
+
+console.log ('Multiline code');
+
+console.log (str1 +'\n'+ 'Testing Javascript code');
---
+++ Advanced JavaScript Assignments/2-Template-Literals/index.js
@@ -0,0 +1,25 @@
+// Assignment
+
+// - Write a cover letter introducing yourself
+// ```use personal information through variable```
+// - create some varibale coverLetter, userName , date , etc
+// - print coverLetter value in console.log() ```print multi-line string literals in console.log()```
+// False
+
+var cons = 'Assalam o alaikum';
+var name ='Rehan Azher';
+var city = 'Lahore';
+var edu = 'Master in Urdu';
+var pEdc = 'M. Ed';
+var profession = 'Teacher';
+
+
+console.log(
+cons + '\n'+
+'May name is ' + name + '\n'+
+'I live in ' + city + '\n'+
+'My education is ' + edu +' and ' + pEdc +'\n'+
+'I am ' + profession + ' by profession'
+
+);
+
